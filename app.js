a3c20107d8a482aa8c023536eef6132a6845e117
kawaragiid <kowachankeigo@gmail.com>
2024-05-30 11:45:32 +0700

    Fix proxy and mixed content issue

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const m3u8url = require('./m3u8url');
 const axios = require('axios');
+require('http-proxy-middleware');
 
 const app = express();
 const port = 3000;
@@ -12,13 +13,33 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Tambahkan rute untuk favicon.ico
+app.get('/favicon.ico', (req, res) => res.status(204));
+// Middleware untuk proxy request
+// Middleware untuk proxy request
+app.get('/proxy', async (req, res) => {
+    try {
+        const url = req.query.url;
+        const response = await axios({
+            method: 'get',
+            url: url,
+            responseType: 'stream'
+        });
+        response.data.pipe(res);
+    } catch (error) {
+        console.error('Error fetching the URL:', error);
+        res.sendStatus(500);
+    }
+});
 app.get('/', async (req, res) => {
     try {
+        console.log('Fetching M3U8 URL:', m3u8url);
         const response = await axios.get(m3u8url);
         const playlist = response.data.split('\n').filter(line => line.trim().startsWith('http'));
+        console.log('Playlist:', playlist); // Tambahkan log ini
         res.render('index', { playlist });
     } catch (error) {
-        console.error('Error fetching the M3U file:', error);
+        console.error('Error fetching the M3U file:', error.message);
         res.sendStatus(500);
     }
 });
@@ -26,3 +47,4 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
 });
+
