b8a4b078630477ff59889f103b6e036d2d70af72
kawaragiid <kowachankeigo@gmail.com>
2024-06-02 23:18:48 +0700

    update

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// app.js
 const express = require('express');
 const path = require('path');
 const m3u8url = require('./m3u8url');
 const axios = require('axios');
-require('http-proxy-middleware');
-const HttpsProxyAgent = require('https-proxy-agent');
 
 
 const app = express();
@@ -15,25 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Tambahkan rute untuk favicon.ico
-app.get('/favicon.ico', (req, res) => res.status(204));
-// Middleware untuk proxy request
-app.get('/proxy', async (req, res) => {
-    try {
-        const url = req.query.url;
-        const agent = new HttpsProxyAgent('https://your-proxy-url'); // URL Proxy HTTPS Anda
-        const response = await axios({
-            method: 'get',
-            url: url,
-            responseType: 'stream',
-            httpsAgent: agent
-        });
-        response.data.pipe(res);
-    } catch (error) {
-        console.error('Error fetching the URL:', error);
-        res.sendStatus(500);
-    }
-});
+
+
 app.get('/', async (req, res) => {
     try {
         console.log('Fetching M3U8 URL:', m3u8url);
@@ -50,4 +30,3 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
 });
-
